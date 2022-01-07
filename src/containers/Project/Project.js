3ffb205ff3fcf00598bb574da38c0e49842fbd77
canalog <alicecha719@naver.com>
2022-01-07 21:16:41 +0900

    Try showing link to other projects -- need improvement

--- src/containers/Project/Project.js
+++ src/containers/Project/Project.js
@@ -6,7 +6,12 @@ const Project = () => {
   return (
     <div className="project-main">
       <h1 className="project-title">Projects</h1>
-      <ul className="skills-list"></ul>
+      <ul className="project-list">
+        <li>
+          {/* 리액트는 다르게 움직이는 것 같음... 더 알아봐야 할 듯 */}
+          <a href="./index.html">Link</a>
+        </li>
+      </ul>
     </div>
   );
 };
